2ea5a881505356a8833a009736b3122dae5f1806
Tanmay Singal <tanmaysingal@icloud.com>
2021-12-04 13:17:49 +0530

    Trained the model

--- mnist_example.py
+++ mnist_example.py
@@ -1,9 +1,16 @@
 # https://www.youtube.com/watch?v=cPmjQ9V6Hbk&list=WL&index=10&t=4131s
 # conda activate tf2_venv && python3 mnist_example.py
 
-import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow
+from tensorflow.keras.layers import (Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D, )
+
+# tensorflow.keras.Sequential
+model = tensorflow.keras.Sequential(
+    [Input(shape=(28, 28, 1)), Conv2D(32, (3, 3), activation="relu"), Conv2D(64, (3, 3), activation="relu"),
+     MaxPool2D(), BatchNormalization(), Conv2D(128, (3, 3), activation="relu"), MaxPool2D(), BatchNormalization(),
+     GlobalAvgPool2D(), Dense(64, activation="relu"), Dense(10, activation="softmax"), ])
 
 
 def display_some_examples(examples, labels):
@@ -29,4 +36,18 @@ if __name__ == "__main__":
     print("x_test.shape = ", x_test.shape)
     print("y_test.shape = ", y_test.shape)
 
-    display_some_examples(x_train, y_train)
+    # display_some_examples(x_train, y_train)
+
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    # one-hot encoding
+    y_train = tensorflow.keras.utils.to_categorical(y_train, 10)
+    y_test = tensorflow.keras.utils.to_categorical(y_test, 10)
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
+    model.fit(x_train, y_train, batch_size=64, epochs=3, validation_split=0.2)
+    model.evaluate(x_test, y_test, batch_size=64)
